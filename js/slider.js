6a494fd733c8e6d25a273be786c5a73d5dc3dce7
fahadismail95 <mfahad605@outlook.com>
2021-12-31 06:21:42 +0500

    app_screens

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,10 @@
-var slideIndex = [1, 1];
+var slideIndex = [1, 1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
+
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
